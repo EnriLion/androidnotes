4a865bf11e797cacf81535e3fb0fd752688f5af1
EnriLion <enriqueleon0201leon@gmail.com>
2023-07-26 10:05:21 -0600

    Updating files

--- koltin_in_action/example.kt
+++ koltin_in_action/example.kt
@@ -1,10 +1,4 @@
-data class Person(val name: String, val age: Int? = null)
-
-fun main(args: Array<String>) {
-    val persons = listOf(Person("Alice"),
-                         Person("Bob", age = 29))
-    val oldest = persons.maxBy { it.age ?: 0 }
-    println("The oldest is: $oldest")
+fun main()
+{
+	println("Iu")
 }
-
-// The oldest is: Person(name=Bob, age=29)
